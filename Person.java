ad8a58bcbb933cc1c641ee86fc1dca78cd8f9f0c
G.H. Miranda <74071824+young-g12@users.noreply.github.com>
2026-09-06 18:41:49 -0400

    Add height attribute and BMR calculation methods
    Added height attribute and methods for height management and BMR calculation.

--- Person.java
+++ Person.java
@@ -2,6 +2,7 @@ public class Person {
 	
 	int age;
 	double weight;
+	double height;
 
 	public Person(){};
 
@@ -20,5 +21,24 @@ public class Person {
 	public double getWeight() {
 		return weight;
 	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+ 	}
+
+	public double bmrConversion(int age, double weight, double height) {
+		double total = (4.536 *(weight)) +(15.875 * (height)) - (5 *(age)) + 5;
+		return total;
+	}
+
+	public double tdee() {
+		bmrConversion(age, weight, height) * int ActivityFactor;
+	}
+
+
 
 }
